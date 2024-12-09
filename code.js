fdb0ad273bdd72da805d7bb383550b5ef068a3c5
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-09 13:18:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -4,33 +4,32 @@ function parallelMergesort(array) {
         return array;
     }
     
-        for (let size = 1; size <=arrsize - 1; size = 2 * size) {
-            array = array.map ((_,i) => {
+        for (let size = 1; size < arrsize - 1; size = 2 * size) {
+            array = array
+                .map ((_,i) => {
                 let left = i * 2 * size;
                 if (left >= arrsize) return null;
                 let mid = Math.min(left + size - 1, arrsize - 1);
                 let right = Math.min(left + 2 * size - 1, arrsize - 1);
                 return merge(array, left, mid, right);
-            }).filter(x => x !== null); 
+            })
+                .filter(x => x !== null); 
+            array = array.reduce((acc, chunk) => acc.concat(chunk), []);
         }  
     return array;
 }
 
-function merge(subarr, left, mid, right){
-     let temp = [];
-    let i = left, j = mid + 1;
-
-    while (i <= mid && j <= right) {
-        if (subarr[i] <= subarr[j]) {
-            temp.push(subarr[i++]);
+function merge(left, right) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            result.push(left[i++]);
         } else {
-            temp.push(subarr[j++]);
+            result.push(right[j++]);
         }
     }
 
-    while (i <= mid) temp.push(subarr[i++]);
-    while (j <= right) temp.push(subarr[j++]);
-    for (let k = 0; k < temp.length; k++) {
-        subarr[left + k] = temp[k];
-    }
+    return result.concat(left.slice(i)).concat(right.slice(j));
 }
