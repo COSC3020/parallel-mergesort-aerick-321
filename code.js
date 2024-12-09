a44ba050a9f1995383aaacbb5e496e12e779ec3a
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-09 12:43:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,16 +2,16 @@ function mergesort(array) {
     if (array.length < 2){
         return array;
     }
-    array.map(function(){
-        for (let size = 1; size <=array.length - 1; size = 2 * size) {
-            for (let left = 0; left < array.length - 1; left += 2 * size){
-               let mid = Math.min(left + size - 1, array.length - 1);
-                let right = Math.min(left + 2 * size - 1, array.length - 1);
-                merge(array, left, mid, right)
-            }
-        }    
-    }
     
+        for (let size = 1; size <=array.length - 1; size = 2 * size) {
+            array = array.map ((_,i) => {
+                let left = i * 2 * size;
+                if (left >= n) return null;
+                let mid = Math.min(left + size - 1, array.length - 1);
+                let right = Math.min(left + 2 * size - 1, array.length - 1);
+                return merge(array, left, mid, right);
+            }).filter(x => x !== null); 
+        }  
     return array;
 }
 
