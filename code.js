5995dfe1d8a3f4e1e2e46362dd10944e31d516be
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-09 12:25:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -17,19 +17,21 @@ function mergesort(array) {
 
 function merge(subarr, left, mid, right){
     let top = mid +1;
-    while(left <= mid && top <= right){
-        if (subarr[left] <= subarr[top]){
-           left++;
-        }
-        else{
-          let value = subarr[top];
-            for(let i = top; i > left; i--){
-                subarr[i] = subarr[i-1];
+       subarr.reduce(){
+           while(left <= mid && top <= right){
+            if (subarr[left] <= subarr[top]){
+               left++;
             }
-            subarr[left] = value;
-            left++;
-            mid++;
-            top++;
+            else{
+              let value = subarr[top];
+                for(let i = top; i > left; i--){
+                    subarr[i] = subarr[i-1];
+                }   
+                subarr[left] = value;
+                left++;
+                mid++;
+                top++;
+            }
         }
     }
 }
