34982bb07daa4662f89ce1a8bfd5429843acb792
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-10 13:17:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,31 +1,37 @@
-function parallelMergesort(array) {
-   if (array.length <= 1) {
-        return Promise.resolve(array);
+async function parallelmergesort(array) {
+    if (array.length < 2){
+        return array;
     }
-
-    const mid = Math.floor(array.length / 2);
-    const left = array.slice(0, mid);
-    const right = array.slice(mid);
-
-    return Promise.all([
-        parallelMergesort(left),
-        parallelMergesort(right)
-    ]).then(([leftSorted, rightSorted]) => {
-        return merge(leftSorted, rightSorted);
-    });
+        for (let size = 1; size <=array.length - 1; size = 2 * size) {
+           const mergeprom = [];
+            for (let left = 0; left < array.length - 1; left += 2 * size){
+               let mid = Math.min(left + size - 1, array.length - 1);
+                let right = Math.min(left + 2 * size - 1, array.length - 1);
+                mergeprom.push(merge(array, left, mid, right));
+            }
+           await Promise.all(mergeprom);
+        }    
+    return array;
 }
 
-function merge(left, right) {
-    let result = [];
-    let i = 0;
-    let j = 0;
-    while (i < left.length && j < right.length) {
-        if (left[i] <= right[j]) {
-            result.push(left[i++]);
-        } else {
-            result.push(right[j++]);
-        }
-    }
-
-    return result.concat(left.slice(i)).concat(right.slice(j));
+async function merge(subarr, left, mid, right){
+   return new Promise((resolve) => {
+      let top = mid +1;
+      while(left <= mid && top <= right){
+         if (subarr[left] <= subarr[top]){
+            left++;
+         }
+         else{
+          let value = subarr[top];
+            for(let i = top; i > left; i--){
+                subarr[i] = subarr[i-1];
+            }
+            subarr[left] = value;
+            left++;
+            mid++;
+            top++;
+           }
+       }
+       resolve();
+    });
 }
